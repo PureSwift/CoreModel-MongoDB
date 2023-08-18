78eb204ecac1944197464690c1d5db06a532c641
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-17 22:49:52 -0400

    Updated unit tests

--- Tests/CoreModelMongoDBTests/MongoDBModelTests.swift
+++ Tests/CoreModelMongoDBTests/MongoDBModelTests.swift
@@ -1,11 +1,48 @@
 import Foundation
 import XCTest
-import CoreModel
+import NIO
 @testable import MongoDBModel
 
 final class MongoDBModelTests: XCTestCase {
     
-    func testExample() throws {
+    func testMongoDB() async throws {
         
+        let elg = MultiThreadedEventLoopGroup(numberOfThreads: 4)
+        let client = try MongoClient("mongodb://localhost:27017", using: elg)
+        let database = client.db("test")
+        
+        defer {
+            // clean up driver resources
+            try? client.syncClose()
+            cleanupMongoSwift()
+
+            // shut down EventLoopGroup
+            try? elg.syncShutdownGracefully()
+        }
+        
+        let model = Model(entities: Person.self, Event.self)
+        let store = MongoModelStorage(
+            database: database,
+            model: model
+        )
+        
+        var person1 = Person(
+            name: "John Appleseed",
+            age: 22
+        )
+        
+        try await store.insert(person1)
+        
+        var event1 = Event(
+            name: "WWDC",
+            date: Date(timeIntervalSinceNow: 60 * 60 * 24 * 10),
+            people: [person1.id]
+        )
+        
+        try await store.insert(event1)
+        person1 = try await store.fetch(Person.self, for: person1.id)!
+        XCTAssertEqual(person1.events, [event1.id])
+        event1 = try await store.fetch(Event.self, for: event1.id)!
+        XCTAssertEqual(event1.people, [person1.id])
     }
 }
