f2ac23c0c5aa0cb1d4ea027bf6442e209dad62a2
Alsey Coleman Miller <alseycmiller@gmail.com>
2023-08-21 01:08:45 -0400

    Add `LogicalQueryOperator`

---
+++ Sources/MongoDBModel/LogicalQueryOperator.swift
@@ -0,0 +1,45 @@
+//
+//  LogicalQueryOperator.swift
+//  
+//
+//  Created by Alsey Coleman Miller on 8/21/23.
+//
+
+import Foundation
+import Predicate
+
+/// Logical Query Operators
+///
+/// Logical operators return data based on expressions that evaluate to true or false.
+///
+/// [Documentation](https://www.mongodb.com/docs/v7.0/reference/operator/query-comparison/)
+public enum LogicalQueryOperator: String, Codable, CaseIterable {
+    
+    /// Joins query clauses with a logical AND returns all documents that match the conditions of both clauses.
+    case and                = "$and"
+    
+    /// Joins query clauses with a logical OR returns all documents that match the conditions of either clause.
+    case or                 = "$or"
+    
+    /// Inverts the effect of a query expression and returns documents that do not match the query expression.
+    case not                = "$not"
+    
+    /// Joins query clauses with a logical NOR returns all documents that fail to match both clauses.
+    case nor                = "$nor"
+}
+
+// MARK: - Predicate
+
+public extension LogicalQueryOperator {
+    
+    init(predicate: Compound.Logical​Type) {
+        switch predicate {
+        case .and:
+            self = .and
+        case .or:
+            self = .or
+        case .not:
+            self = .not
+        }
+    }
+}
